ea5fa4b0f9c8c79fd08b4939f563444d5cc153b6
Ivan Pidov <ivan.pidov@tick42.com>
2018-01-30 18:01:40 +0200

    add cors config

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ const bodyParser = require('body-parser')
 const passport = require('passport')
 const LdapStrategy = require('passport-ldapauth')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const { User } = require('./auth/User')
 const { ldap, bearer } = require('./auth')
+
+var corsOptions = {
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true
+}
 
 mongoose.connect(config.mongodb.url).then(db => {
   passport.use(new LdapStrategy(config.ldap, User.upsertLdapUser.bind(User)))
 
   const app = express()
 
+  app.use(cors(corsOptions))
   app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(bodyParser.urlencoded({ extended: true }))
   app.use(passport.initialize())
 
   app.use('/login', ldap(passport))
